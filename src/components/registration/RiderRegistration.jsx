c5622ccf4345a319b0ec99eec9ac71a5303da0bb
Abheelash-Mishra <abheelashdev@gmail.com>
2025-04-09 12:08:05 +0530

    Block rider registration form if already registered

--- src/components/registration/RiderRegistration.jsx
+++ src/components/registration/RiderRegistration.jsx
@@ -1,10 +1,20 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
 const RiderRegistration = () => {
+    const [showForm, setShowForm] = useState(true);
     const [email, setEmail] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
+
+    useEffect(() => {
+        const storedValue = localStorage.getItem("RiderID");
+        if (storedValue === null) {
+            setShowForm(true);
+        } else {
+            setShowForm(false);
+        }
+    }, []);
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -56,37 +66,47 @@ const RiderRegistration = () => {
             <h1 className="text-2xl font-semibold mb-8">
                 Register as a Rider
             </h1>
-            <div className="flex items-center justify-center w-full">
-                <form
-                    onSubmit={handleSubmit}
-                    className="grid grid-cols-4 gap-4 w-2/3 justify-center items-center"
-                >
-                    <label className="col-span-2 text-md font-medium text-right">Email ID:</label>
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={handleEmailChange}
-                        className="col-span-2 px-2 py-1 rounded-lg border-black/80 border-2"
-                    />
-
-                    <label className="col-span-2 text-md font-medium text-right">Phone Number:</label>
-                    <input
-                        type="text"
-                        value={phoneNumber}
-                        onChange={handlePhoneNumberChange}
-                        className="col-span-2 px-2 py-1 rounded-lg border-black/80 border-2"
-                    />
-
-                    <div className="col-span-4 flex justify-center mt-2">
-                        <button
-                            type="submit"
-                            className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800"
+            {showForm ? (
+                <>
+                    <div className="flex items-center justify-center w-full">
+                        <form
+                            onSubmit={handleSubmit}
+                            className="grid grid-cols-4 gap-4 w-2/3 justify-center items-center"
                         >
-                            Register
-                        </button>
+                            <label className="col-span-2 text-md font-medium text-right">Email ID:</label>
+                            <input
+                                type="text"
+                                value={email}
+                                onChange={handleEmailChange}
+                                className="col-span-2 px-2 py-1 rounded-lg border-black/80 border-2"
+                            />
+
+                            <label className="col-span-2 text-md font-medium text-right">Phone Number:</label>
+                            <input
+                                type="text"
+                                value={phoneNumber}
+                                onChange={handlePhoneNumberChange}
+                                className="col-span-2 px-2 py-1 rounded-lg border-black/80 border-2"
+                            />
+
+                            <div className="col-span-4 flex justify-center mt-2">
+                                <button
+                                    type="submit"
+                                    className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800"
+                                >
+                                    Register
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
+                </>
+            ) : (
+                <>
+                    <h1 className={"text-lg font-semibold"}>You have already registered!</h1>
+                    <h2 className={"text-lg font-semibold"}>Your unique ID number is '{localStorage.getItem("RiderID")}'</h2>
+                </>
+            )}
+
         </div>
     );
 };
