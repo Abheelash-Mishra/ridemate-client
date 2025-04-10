6c6d2ac556577263a7279f6c8e77213ac09edf3e
Abheelash-Mishra <abheelashdev@gmail.com>
2025-04-10 12:55:35 +0530

    Fail fast if env variables are not set

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import {Toaster} from "react-hot-toast";
 
+const requiredEnvVars = ["VITE_BASE_URL"];
+
+for (const key of requiredEnvVars) {
+    if (!import.meta.env[key]) {
+        alert(`Missing required environment variable: ${key}`);
+        throw new Error(`Missing required environment variable: ${key}`);
+    }
+}
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Toaster />
