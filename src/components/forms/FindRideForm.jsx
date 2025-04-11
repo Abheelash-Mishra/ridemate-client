d0e0cf4cd43f1349876e7ad96e7c21cf8573faae
Abheelash-Mishra <abheelashdev@gmail.com>
2025-04-11 12:03:24 +0530

    Store matched drivers and block form when a ride is ongoing

--- src/components/forms/FindRideForm.jsx
+++ src/components/forms/FindRideForm.jsx
@@ -1,10 +1,19 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
 const FindRideForm = () => {
     const [matchedDrivers, setMatchedDrivers] = useState(null);
     const [rideStatus, setRideStatus] = useState(null);
+    const [showForm, setShowForm] = useState(true);
+
+    useEffect(() => {
+        setMatchedDrivers(JSON.parse(localStorage.getItem("matchedDrivers")));
+
+        if (localStorage.getItem("RideID") !== null) {
+            setShowForm(false);
+        }
+    }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -27,7 +36,11 @@ const FindRideForm = () => {
         });
 
         request.then(response => {
-            setMatchedDrivers(response.data?.matchedDrivers ?? null);
+            setMatchedDrivers(response.data?.matchedDrivers);
+
+            if (response.data?.matchedDrivers.length > 0) {
+                localStorage.setItem("matchedDrivers", JSON.stringify(response.data?.matchedDrivers));
+            }
         }).catch(error => console.error(error.response));
     };
 
@@ -42,6 +55,7 @@ const FindRideForm = () => {
         }).then(response => {
             localStorage.setItem("RideID", response.data.rideID);
             localStorage.setItem("DriverID", response.data.driverID);
+            localStorage.removeItem("matchedDrivers");
             setRideStatus(response.data);
 
             toast.success("Ride Started Successfully!");
@@ -53,55 +67,60 @@ const FindRideForm = () => {
 
     return (
         <div className={"h-[28rem] flex flex-col justify-center items-center text-xl font-bold"}>
-            {!rideStatus ? (
-                <>
-                    <form
-                        onSubmit={handleSubmit}
-                        className="flex flex-col w-2/3 justify-center items-center"
-                    >
-                        <h2 className={"my-8 justify-center items-center"}>Ready to Find Nearby Drivers?</h2>
+            { showForm ? (
+                !rideStatus ? (
+                    <>
+                        <form
+                            onSubmit={handleSubmit}
+                            className="flex flex-col w-2/3 justify-center items-center"
+                        >
+                            <h2 className={"my-8 justify-center items-center"}>Ready to Find Nearby Drivers?</h2>
 
-                        <div className="flex justify-center mt-2">
-                            <button
-                                type="submit"
-                                className="text-lg bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800"
-                            >
-                                Search for Drivers
-                            </button>
+                            <div className="flex justify-center mt-2">
+                                <button
+                                    type="submit"
+                                    disabled={matchedDrivers !== null && matchedDrivers.length > 0}
+                                    className="text-lg bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 disabled:bg-gray-500"
+                                >
+                                    Search for Drivers
+                                </button>
+                            </div>
+                        </form>
+
+                        <div className={"my-8"}>
+                            {matchedDrivers !== null ? (
+                                matchedDrivers.length > 0 ? (
+                                    <div className={"flex flex-col items-center"}>
+                                        <h1 className={"mb-4"}>Select a driver (Ordered closest to furthest)</h1>
+                                        {matchedDrivers.map((driver, index) => (
+                                            <p
+                                                key={index}
+                                                className={"text-blue-600 cursor-pointer font-medium hover:text-blue-900"}
+                                                onClick={(e) => handleStartRide(e, index+1)}
+                                            >
+                                                {index+1}. DRIVER_D{driver}
+                                            </p>
+                                        ))}
+                                    </div>
+                                ) : (
+                                    <p>No Drivers Available.</p>
+                                )
+                            ) : null}
                         </div>
-                    </form>
-
-                    <div className={"my-8"}>
-                        {matchedDrivers !== null ? (
-                            matchedDrivers.length > 0 ? (
-                                <div className={"flex flex-col items-center"}>
-                                    <h1 className={"mb-4"}>Select a driver (Ordered closest to furthest)</h1>
-                                    {matchedDrivers.map((driver, index) => (
-                                        <p
-                                            key={index}
-                                            className={"text-blue-600 cursor-pointer font-medium hover:text-blue-900"}
-                                            onClick={(e) => handleStartRide(e, index+1)}
-                                        >
-                                            {index+1}. DRIVER_D{driver}
-                                        </p>
-                                    ))}
-                                </div>
-                            ) : (
-                                <p>No Drivers Available.</p>
-                            )
-                        ) : null}
+                    </>
+                ) : (
+                    <div className="mt-8 mb-4 bg-gray-200 rounded-xl flex flex-col items-center">
+                        <h1 className="my-4">Your Ride Details</h1>
+                        <div className="grid grid-cols-2 gap-4 font-medium">
+                            <div className="p-4 text-center">Ride ID: {rideStatus.rideID}</div>
+                            <div className="p-4 text-center">Rider ID: {rideStatus.riderID}</div>
+                            <div className="p-4 text-center">Driver ID: {rideStatus.driverID}</div>
+                            <div className="p-4 text-center">Status: {rideStatus.status}</div>
+                        </div>
                     </div>
-                </>
+                )
             ) : (
-                <div className="mt-8 mb-4 bg-gray-200 rounded-xl flex flex-col items-center">
-                    <h1 className="my-4">Your Ride Details</h1>
-                    <div className="grid grid-cols-2 gap-4 font-medium">
-                        <div className="p-4 text-center">Ride ID: {rideStatus.rideID}</div>
-                        <div className="p-4 text-center">Rider ID: {rideStatus.riderID}</div>
-                        <div className="p-4 text-center">Driver ID: {rideStatus.driverID}</div>
-                        <div className="p-4 text-center">Status: {rideStatus.status}</div>
-                    </div>
-                </div>
+                <p>You already have a ride that's incomplete!</p>
             )}
         </div>
     );
