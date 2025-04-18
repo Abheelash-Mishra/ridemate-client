28d9f410ddfebd59a7df5ec9e932a9ae413516e0
Abheelash-Mishra <abheelashdev@gmail.com>
2025-04-18 15:30:58 +0530

    Move the driver registration form to its own page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import RideServices from "./components/RideServices";
 import AdminServices from "./components/AdminServices.jsx";
 import {useEffect} from "react";
+import DriverRegistration from "./components/registration/DriverRegistration.jsx";
 
 function App() {
     useEffect(() => {
@@ -23,6 +24,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route index element={<Homepage />} />
+                    <Route path={"register/driver"} element={<DriverRegistration />} />
                     <Route path={"rides"} element={<RideServices />} />
                     <Route path={"admin"} element={<AdminServices />} />
                 </Routes>
